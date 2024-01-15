9cea21dd1c05ec928d346639d3235b62918a58b4
Jorge Rios <jorgeluisrios47@gmail.com>
2024-01-15 12:34:34 -0600

    added form to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import Form from './components/Form'
-function App() {
+import { useState } from "react";
 
+function App() {
+  const [title, setTitle] = useState("");
+  function handleInput (e){
+    e.preventDefault();
+
+    fetch('http://localhost:3000/post',{
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": 'application/json',
+      }
+    })
+
+  }
   return (
-    <>
-     <Form />
-    </>
-  )
+    <div className="App">
+      <form onSubmit={handleInput}>
+        <label htmlFor="">Title</label>
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
